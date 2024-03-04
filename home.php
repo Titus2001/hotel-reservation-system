22517bf04a7f5832a02d8915337d92de17d6b209
Titus2001 <140101946+Titus2001@users.noreply.github.com>
2024-03-04 16:46:38 +0300

    Update home.php

--- home.php
+++ home.php
@@ -17,10 +17,10 @@
 
 	<div class="container-fluid">
 		<div class =  "row">
-			<p>Hotel Four Seasons, among the premier business hotel in amreli the economic capital of saurashtra is characterised by the traditionally hospitality located in the heart of the city.The ideal choice amongs hotels in Amreli for businessman and tourist. </p>
+			<p>Pine Hotel , among the premier business hotel in amreli the economic capital of saurashtra is characterised by the traditionally hospitality located in the heart of the city.The ideal choice amongs hotels in Amreli for businessman and tourist. </p>
 		</div>
 		<div class="row">
-			<p>4Seasons offers facilities that no other Amreli hotels offer. Each room is elegantly well appointed and equipped with complete amenities including aircondition fully stock minibars,directIDD telephone,TV with 24 hour satelight entertainment and news.Upscale bathroom with hair dryers and cosmetic mirrors,seperate hanging closets.</p>
+			<p>Pine Hotel offers facilities that no other Amreli hotels offer. Each room is elegantly well appointed and equipped with complete amenities including aircondition fully stock minibars,directIDD telephone,TV with 24 hour satelight entertainment and news.Upscale bathroom with hair dryers and cosmetic mirrors,seperate hanging closets.</p>
 		</div>
 		<div class="row">
 			<p>The only one Amreli hotels having all facilities a business center,beauty parlor,health club,gym,saloon, boutique,internet cafe,pastry shop,travel desk and money exchange the PARAS hotels given you full value for money.</p>
